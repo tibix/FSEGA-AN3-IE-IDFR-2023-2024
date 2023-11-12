fdba098896e03edfb5910f6a63c3ad1cd88ef15b
Tiberiu Dascal <tiberiu.purcel@gmail.com>
2023-11-12 15:44:53 +0200

    Sync

--- SEM_1/Proiectarea_Siteuri_Web/Tema2/Pas 6/index.php
+++ SEM_1/Proiectarea_Siteuri_Web/Tema2/Pas 6/index.php
@@ -1,10 +1,14 @@
 <?php
 session_start();
+
 // Includeți funcții și conectați-vă la baza de date folosind PDO MySQL
 include 'conectare.php';
 $pdo = pdo_connect_mysql();
-// hom este pgina start
+
+// home este pgina start
 $page = isset($_GET['page']) && file_exists($_GET['page'] . '.php') ? $_GET['page'] : 'home';
+
 // Includeți și afișați pagina solicitată
 include $page . '.php';
+
 ?>
